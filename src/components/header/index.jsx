f185cede0c97ea69dbaee99e6d4c9de44e1ed918
nickzmoro <nicolasmororuiz@gmail.com>
2025-05-15 21:16:35 -0300

    feat: header component added with all styles and optimization for all pages

---
+++ src/components/header/index.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header className="fixed top-0 w-screen h-[7vh] flex justify-center items-center z-50">
+      <div>
+        <a href="#" className="uppercase font-[600] text-[#fff] text-[1.5rem]">
+          <span className="text-[#707070] font-[500]">&lt;</span> Dev News{" "}
+          <span className="text-[#707070] font-[500]">/&gt;</span>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Header from "./components/header/index.jsx";
+import "./styles/styles.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
