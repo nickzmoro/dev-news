550825a3b0a74bfffdd0a51cde54bef6601b86d5
nickzmoro <nicolasmororuiz@gmail.com>
2025-05-20 21:43:44 -0300

    feat: implemented a search modal in the header with input to after implement the logic

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,46 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
+import { IoClose } from "react-icons/io5";
+
 const Header = () => {
+  const [modalSearch, setModalSearch] = useState(false);
+
   return (
-    <header className="fixed top-0 w-screen h-[7vh] flex justify-center items-center z-50 backdrop-blur-[15px] border-b border-b-[#ffffff1e]">
-      <div>
-        <a href="#" className="uppercase font-[600] text-[#fff] text-[1.5rem]">
-          <span className="text-[#707070] font-[500]">&lt;</span> Dev News{" "}
-          <span className="text-[#707070] font-[500]">/&gt;</span>
-        </a>
-      </div>
-    </header>
+    <>
+      <header className="fixed top-0 w-screen h-[7vh] flex justify-center items-center z-50 backdrop-blur-[15px] border-b border-b-[#ffffff1e]">
+        <div className="relative">
+          <button
+            className="w-[500px] py-1 px-3 flex gap-2 items-center border border-[#ffffff3b] rounded-[12px] text-[#ffffff98] cursor-pointer hover:shadow-[0_0_10px_#ffffff1a] transition-all duration-300"
+            onClick={() => setModalSearch(true)}
+          >
+            <ImSearch size={14} /> Procurar por artigos.. (react, javascript,
+            etc)
+          </button>
+        </div>
+      </header>
+      {modalSearch && (
+        <div
+          onClick={() => setModalSearch(false)}
+          className="fixed top-[0] left-0 w-screen h-screen bg-[#000000a9] flex items-center justify-center z-[100]"
+        >
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="p-4 px-6 bg-[#ffffff0a] backdrop-blur-[5px] rounded-[12px] border border-[#ffffff38] flex items-center gap-4"
+          >
+            <ImSearch size={18} color="#aaaaaa" />
+            <input
+              type="text"
+              placeholder="Procurar..."
+              className="outline-none text-[1.1rem] text-[#fff] w-[500px]"
+              name="search"
+            />
+            <div className="bg-[#303030] rounded-[12px] p-[2px] cursor-pointer hover:bg-[#383838]">
+              <IoClose size={20} color="#aaaaaa" />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
