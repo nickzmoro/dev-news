4b872677afd1f8bca40ffa1938bb2190d591f86e
nickzmoro <nicolasmororuiz@gmail.com>
2025-05-22 21:02:32 -0300

    style: styles implemented in header and modal search; responsiveness for any screen

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -24,7 +24,7 @@ const Header = ({ article }) => {
         <header className="fixed top-0 w-screen h-[7vh] flex justify-center items-center z-50 backdrop-blur-[15px] border-b border-b-[#ffffff1e]">
           <div className="relative">
             <button
-              className="w-[500px] py-1 px-3 flex gap-2 items-center border border-[#ffffff3b] rounded-[12px] text-[#ffffff98] cursor-pointer hover:shadow-[0_0_10px_#ffffff1a] transition-all duration-300"
+              className="w-[500px] max-md:w-full py-1 px-3 flex gap-2 items-center border border-[#ffffff3b] rounded-[12px] text-[#ffffff98] cursor-pointer hover:shadow-[0_0_10px_#ffffff1a] transition-all duration-300"
               onClick={() => setModalSearch(true)}
             >
               <ImSearch size={14} /> Procurar por artigos.. (react, javascript,
@@ -37,18 +37,18 @@ const Header = ({ article }) => {
       {modalSearch && (
         <div
           onClick={() => setModalSearch(false)}
-          className="fixed top-[0] left-0 w-screen h-screen bg-[#000000a9] flex items-center justify-center z-[100]"
+          className="fixed top-[0] left-0 w-screen h-screen bg-[#000000a9] flex items-center justify-center z-[100] max-md:px-5"
         >
           <div
             onClick={(e) => e.stopPropagation()}
-            className="p-4 px-6 bg-[#ffffff0a] backdrop-blur-[5px] rounded-[12px] border border-[#ffffff38]"
+            className="p-4 px-6 bg-[#ffffff0a] backdrop-blur-[15px] rounded-[12px] border border-[#ffffff38] w-[600px] max-md:w-full"
           >
             <div className="flex items-center gap-4">
               <ImSearch size={18} color="#aaaaaa" />
               <input
                 type="text"
                 placeholder="Procurar..."
-                className="outline-none text-[1.1rem] text-[#fff] w-[500px]"
+                className="outline-none text-[1.1rem] text-[#fff] w-[500px] max-md:w-full"
                 name="search"
                 value={captureInputValue}
                 onChange={(e) => {
@@ -63,20 +63,44 @@ const Header = ({ article }) => {
                 <IoClose size={20} color="#aaaaaa" />
               </div>
             </div>
-            {searchResult.length > 0
-              ? searchResult.map((item) => (
-                  <div
-                    key={item.id}
-                    className="mt-5 pt-3 border-t border-t-[#fff] text-[#fff]"
-                  >
-                    <p>{item.title}</p>
-                  </div>
-                ))
-              : captureInputValue.trim() !== "" && (
-                  <div className="mt-5 pt-3 border-t border-t-[#fff] text-[#fff]">
-                    <p>Nenhum resultado encontrado..</p>
-                  </div>
-                )}
+            <div className="max-h-[400px] overflow-y-auto mt-3">
+              <div className="w-full h-[1px] bg-[#ffffff42]"></div>
+              {searchResult.length > 0
+                ? searchResult.map((item) => (
+                    <div key={item.id} className="mt-5 text-[#fff] flex gap-3">
+                      <div>
+                        <img
+                          src={item.user.profile_image_90}
+                          alt=""
+                          className="w-[70px] rounded-full border-2 border-[#adadad]"
+                        />
+                      </div>
+                      <div className="flex flex-col gap-0.5">
+                        <div className="flex items-center gap-1">
+                          <a
+                            href={item.url}
+                            className="font-[600] hover:underline hover:text-[#ffffffea]"
+                            target="_blank"
+                            rel="noopener noreferer"
+                          >
+                            {item.user.name}
+                          </a>
+                          <span className="text-[#ffffff98]">
+                            - {item.readable_publish_date}
+                          </span>
+                        </div>
+                        <div>
+                          <p className="text-[#ffffffe5]">{item.description}</p>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                : captureInputValue.trim() !== "" && (
+                    <div className="pt-3 text-[#ffffffb2]">
+                      <p>Nenhum resultado encontrado...</p>
+                    </div>
+                  )}
+            </div>
           </div>
         </div>
       )}
